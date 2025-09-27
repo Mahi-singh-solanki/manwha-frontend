c7f56bc19c36e7743bc974d5b544f74f688473b8
Mahi <mahipalsinghapsit0@gmail.com>
2025-09-27 23:46:23 +0530

    second

--- mahi-manwha/src/api/Api.jsx
+++ mahi-manwha/src/api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiclient=axios.create({
-  baseURL: 'http://localhost:8080', // Your backend's URL
+  baseURL: 'https://mahi-manwha.onrender.com', // Your backend's URL
   headers: {
     'Content-Type': 'application/json',
   },
