0558ba99d3afb3c520b45625c52ef2e27648a0f1
Mahi <mahipalsinghapsit0@gmail.com>
2025-10-02 00:05:43 +0530

    done

--- mahi-manwha/src/api/Api.jsx
+++ mahi-manwha/src/api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiclient=axios.create({
-  baseURL: 'http://localhost:8080', // Your backend's URL
+  baseURL: 'https://manwha-production.up.railway.app', // Your backend's URL
   headers: {
     'Content-Type': 'application/json',
   },
