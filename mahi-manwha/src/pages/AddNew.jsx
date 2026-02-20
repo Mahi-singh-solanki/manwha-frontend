3df2564991764ae55d4a20b7749066a097550dab
Mahipal singh solanki <mahipalsinghapsit0@gmail.com>
2026-02-20 14:34:18 +0530

    Update AddNew.jsx

--- mahi-manwha/src/pages/AddNew.jsx
+++ mahi-manwha/src/pages/AddNew.jsx
@@ -61,7 +61,7 @@ export const AddNew = () => {
             </div>
 
          
-            <div className="relative w-full mb-10 flex gap-2">
+            <div className="relative w-full mb-10 flex flex-col gap-2">
                 <Search className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400" size={18} />
                 <form onSubmit={handleSubmit}>
                 <input
